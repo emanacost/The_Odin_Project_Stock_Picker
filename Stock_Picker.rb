ee69a91e97c7a2c89cfedae7edb0d5491bb8aeab
eacosta <eacosta0411@gmail.com>
2025-10-14 14:56:19 -0400

    Add helper method that will determine the index of the highest value in array and implement it in stock_picker

--- Stock_Picker.rb
+++ Stock_Picker.rb
@@ -3,7 +3,25 @@ def stock_picker(arr)
     # Use map to find the diff of the current iteration to all members of the array
     # Take the highest resulting number and store it
     # If a new iteration has a higher number replace the number
+    adj_arr = arr.map { |val_map| val - val_map}
+    highest_diff = max_index(adj_arr)
   end
+end
+
+def max_index(array)
+  if array.empty?
+    return [] # Return an empty array if the input array is empty.
+  end
+
+  highest_value = array.max
+  indices_of_highest = []
+  array.each_with_index { |element, index|
+    if element == highest_value
+      indices_of_highest << index
+    end
+  }
+
+  return indices_of_highest
 end
 
 
