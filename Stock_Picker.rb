c243f620c0b6b774beb0d99a3de4664b441824fc
eacosta <eacosta0411@gmail.com>
2025-10-14 14:41:42 -0400

    Add comments to stock_picker method as plan

--- Stock_Picker.rb
+++ Stock_Picker.rb
@@ -1,6 +1,8 @@
 def stock_picker(arr)
   arr.each do |val|
-
+    # Use map to find the diff of the current iteration to all members of the array
+    # Take the highest resulting number and store it
+    # If a new iteration has a higher number replace the number
   end
 end
 
